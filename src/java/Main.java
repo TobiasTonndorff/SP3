5b3364c5c5dda5c7f485694926339b9f8fe56810
Marcus Anastasiades <91838917+Maakasu@users.noreply.github.com>
2022-11-03 12:10:13 +0100

    GUI

--- src/java/Main.java
+++ src/java/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         UserManager manager = UserManager.getInstance();
-
         manager.loadUsers();
         System.out.println(manager.hasUser("Morten", "Fede"));
     }
