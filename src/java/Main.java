fafdac9047c800cfc5eeb635858b804e593983a0
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2022-11-03 12:30:09 +0100

    main

--- src/java/Main.java
+++ src/java/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         UserManager manager = UserManager.getInstance();
+
         manager.loadUsers();
         System.out.println(manager.hasUser("Morten", "Fede"));
+
+
+       GUI loginpage = new GUI();
+        loginpage.getClass();
     }
 }
