b2c02c5a2d560b832aecf4b5c85d222e35b57b70
Marcus Anastasiades <91838917+Maakasu@users.noreply.github.com>
2022-11-07 15:15:43 +0100

    Update Main.java

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -1,18 +1,11 @@
 public class Main {
-
     public static void main(String[] args) {
-        MenuNavigation menuNavigation = new MenuNavigation();
-        menuNavigation.loginScreen();
 
-
-       /* UserManager manager = UserManager.getInstance();
+        UserManager manager = UserManager.getInstance();
 
         manager.loadUsers();
-        System.out.println(manager.hasUser("Morten", "Fede"));
-        MainMenu m1 = new MainMenu();
-        m1.menuChoice();*/
 
-
-
+        GUI loginpage = new GUI();
+        loginpage.getClass();
     }
 }
