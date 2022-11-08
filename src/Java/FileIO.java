3ea63ba273d1604c6279be6ae3db1b8c7e0ad069
Tobias Tonndorff <tobias@tonndorff.dk>
2022-11-08 11:08:39 +0100

    FileIO

--- src/Java/FileIO.java
+++ src/Java/FileIO.java
@@ -3,24 +3,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
+
 //VIRKER IKKE
 public class FileIO {
-    public ArrayList<String> readMoviesFile() {
+    /*public Movies readMoviesFile(String field, String query) {
         File file = new File("SP3/src/Data/Movies.txt");
         ArrayList<String> movies = new ArrayList<>();
         try {
             Scanner input = new Scanner(file);
-            input.nextLine();
-            while (input.hasNextLine()) ;
-            movies.add(input.nextLine());
+            String[] header = input.nextLine().split(";");
+            int index = 0;
+            for(int i = 0; i < header.length; i++){
+                if(header[i].equalsIgnoreCase(field)){
+                    index = i;
+                }
+
+            }
+            while(input.hasNextLine()){
+                String movie = input.nextLine();
+                String[] values = movie.split(";");
+                if(values[index].equalsIgnoreCase(query)){
+                    return movie(values);
+                }
+            }
         } catch (FileNotFoundException e) {
             movies = null;
         }
         return movies;
 
+    }*/
+    private Movies movie(String [] m) {
+        Map map = media(m);
+        int year = Integer.parseInt(m[2]);
+        return new Movies((String) map.get("title"), (ArrayList) map.get("categories"), (Float) map.get("rating"), year, (Integer) map.get("ID"));
+    }
+    private Map media(String [] me){
+        int ID = Integer.parseInt(me[0]);
+        String title = me[1];
+        ArrayList<String> categories = new ArrayList<>();
+        String[] categoriesArr = me[3].split(",");
+        categories.addAll(Arrays.asList(categoriesArr));
+        float rating = Float.parseFloat(me[4].replace(",", "."));
+        Map map = new HashMap<>();
+        map.put("title", title);
+        map.put("categories", categories);
+        map.put("rating", rating);
+        map.put("ID", ID);
+        return map;
     }
 
     public ArrayList<String> readSeriesFile() {
