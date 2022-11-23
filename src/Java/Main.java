7a25376136eca3ebc513155693dfab4b4c52d68c
Madsen <112688147+ollemadsen@users.noreply.github.com>
2022-11-24 00:36:32 +0100

    k

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.jdbc.driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/Users","root","olle123");
+            Statement st=con.createStatement();
+            System.out.println("Enter Username");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         UserManager manager = UserManager.getInstance();
 
