3f5f4e703af58a85e12cb302e6cba5dea8d0496f
Madsen <112688147+ollemadsen@users.noreply.github.com>
2022-11-24 01:16:26 +0100

    MySQL database

--- Test/src/TestJDBCConnection.java
+++ Test/src/TestJDBCConnection.java
@@ -1,3 +1,4 @@
+import com.mysql.jdbc.util.ResultSetUtil;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import java.sql.*;
@@ -9,13 +10,20 @@ public class TestJDBCConnection {
     public void  database(){
         Connection connection;
 
-        String url = "jdbc:mysql://localhost/sp3?" + "autoReconnect=true&useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/sp3?" + "autoReconnect=true&useSSL=false";
         String username = "root";
         String password = "olle123";
 
 
         try{
             connection = DriverManager.getConnection(url, username, password);
+            Statement st = connection.createStatement();
+            st.execute("SELECT * FROM movies");
+            ResultSet rs = st.getResultSet();
+            if (rs.next()) {
+
+                System.out.println(rs.getString(1));
+            }
             assertEquals(true, connection.isValid(1));
             connection.close();
             assertEquals(false, connection.isValid(1));
